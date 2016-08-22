f99fb2c44282c2028ae21bcc51fe48bace0d86ac
Edward JE Koch <edward7j@gmail.com>
2016-08-21 22:45:53 -0700

    Immediately Invoked Function Expressions.

---
+++ c03/js/iife.js
@@ -0,0 +1,11 @@
+// The below function is  immediately invoked. The variable does not store the
+//function for later use. Instead, the variable holds the answer returned by the funcion.
+
+var area = (function() {
+
+var width = 3;
+var height = 2;
+return width*height;
+}());
+
+
