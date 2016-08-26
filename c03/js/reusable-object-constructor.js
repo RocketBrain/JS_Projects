ee2bbde5be3137c3701fbdbcae96947010c487ae
Edward JE Koch <edward7j@gmail.com>
2016-08-25 21:24:00 -0700

    Re-usable object-constructor template.

---
+++ c03/js/reusable-object-constructor.js
@@ -0,0 +1,28 @@
+/* The following function demonstrates a re-usable constructor.
+
+Inn (pun intended) this case we have a re-usable object constructor for several
+different types of hotels. Simply by declaring a variable with the 'new' functionality
+and referencing the function, you can re-use this Object-Constructor templates
+as many times as necessary, all with different key-value pairs.
+
+*/  
+
+
+
+function Hotel (name,rooms,booked) {
+
+	this.name = name;
+	this.rooms = rooms;
+	this.booked = booked; 
+
+	this.checkAvailiablility = function () {
+
+		return this.rooms = this.booked; 
+
+
+	}
+}
+	
+
+	
+var exampleHotel = new Hotel ('hotelName', 40, 23);
