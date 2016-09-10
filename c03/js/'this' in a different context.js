f35c0e9ec71d0ce2fb924b6b5d8dc4f68bd83480
Edward JE Koch <edward7j@gmail.com>
2016-09-09 22:52:25 -0700

    'this' used in global, local, and a different context.

---
+++ c03/js/'this' in a different context.js
@@ -0,0 +1,23 @@
+var width = 600;
+var shape = {width:300};
+
+
+var showWidth = function(){
+	document.write(this.width) //the absence of "var" makes this refer to the global variable.
+}
+
+showWidth();
+
+
+
+
+
+function windowSize() {
+	
+	var width = this.innerWidth;
+	var height = this.innerHeight;  //the default object here is the window object.
+	return[height,width]; //'this' is being referred to the global function of windowSize().
+	
+	//the variable width still remains local, however.
+
+}
