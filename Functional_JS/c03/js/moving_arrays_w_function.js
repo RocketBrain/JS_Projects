8b5a64bf23754276cc00c8a822bec64aeae93842
Edward J.E Koch <visceral910@gmail.com>
2016-09-18 20:12:26 -0700

    Manipulating arrays with function.

--- Functional_JS/c03/js/moving_arrays_w_function.js
+++ Functional_JS/c03/js/moving_arrays_w_function.js
@@ -2,14 +2,14 @@
 
 function addPassenger (name,list){
 
-
+	// Checks for person, if 0, adds their name.
 	if (list.length==0){
 
 		list.push(name);
 	} 
 		else 
 		{
-
+			// Goes through entire array, and lists it.
 			for (var i = 0; i < list.lenth; i++){
 
 				if(list[i] == undefined){
