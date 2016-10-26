9e176922fe1e766e4c88e7f5aa2a8fd871c621b9
Edward J.E Koch <visceral910@gmail.com>
2016-10-25 19:50:45 -0700

    Fibonacci sequence using recursion.

--- Functional_JS/programs_notes/fibonacci_sequence_using_recursion.js
+++ Functional_JS/programs_notes/fibonacci_sequence_using_recursion.js
@@ -1,16 +1,17 @@
 //Printing a fibonacci sequence in javascript
 
 
-function Fibonacci(n){
-    this.looping = function (){
+function Fibonacci(){
+    this.looping = function (n){
         var a = 0;
         var b = 1;
-        var f =  1;
+        var f = 1;
 
         for (var i = 2 ; i<=n; i++){
             f = a + b;
             a = b;
             b = f;
+            console.log(i);
         }
         return f;
 
