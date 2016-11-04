cd19cfaba4574797535482c9176527213ea31324
Edward J.E Koch <visceral910@gmail.com>
2016-11-03 23:40:42 -0700

    Using prototype for array with objects.

---
+++ Functional_JS/programs_notes/prototype_using_array_with_objects.js
@@ -0,0 +1,26 @@
+var carMakes = [
+  {name: "BMW", color: "black", mileage: 5000},
+  {name: "TOYOTA", color: "grey", mileage: 15000},
+  {name: "BMW", color: "white", mileage: 50000},
+
+];
+
+
+//a prototype which counts the make of a particular car
+Array.prototype.countMake = function(make){
+  var numMake = 0;
+  //this refers to object that is calling function
+  for(var i = 0; i<this.length; i++){
+    // i goes through entire list of objects inside of array
+    //Once it findes the name property, it then increments the numMake variable.
+    if(  make == this[i].name) {
+      
+      numMake++;
+    }
+  }
+ 
+  return numMake;
+  
+};
+
+carMakes.countMake("BMW")
