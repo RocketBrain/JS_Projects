5ce50778c415eaadacbb340740abd8986d699f24
Edward JE Koch <edward7j@gmail.com>
2016-11-07 21:51:39 -0800

    Relaying coordinates using 'Object Create' between different objects.

--- Functional_JS/programs_notes/using_object_create_between_objects.js
+++ Functional_JS/programs_notes/using_object_create_between_objects.js
@@ -19,6 +19,8 @@ var theCoordinates = {
 
 coordinate1 = Object.create(theCoordinates);
 coordinate2 = Object.create(theCoordinates);
+coordinate3 = Object.create(theCoordinates);
+coordinate4 = Object.create(theCoordinates);
 
 coordinate1.x = 3;
 coordinate1.y = 4;
@@ -28,9 +30,27 @@ coordinate2.x = 8;
 coordinate2.y = 9;
 coordinate2.z = 3;
 
+coordinate3.x = 10;
+coordinate3.y = 8;
+coordinate3.z = 4;
+
+coordinate4.x = 12;
+coordinate4.y = 7;
+coordinate4.z = 2;
+
 
 
 coordinate1.sendCoordinatesTo(coordinate2);
 coordinate2.sendCoordinatesTo(coordinate1);
 
+coordinatesArray = [coordinate1,coordinate2,coordinate3,coordinate4];
 
+for(i = 0; i<coordinatesArray.length; i ++){
+if(coordinatesArray[i].y % 2 === 0 ){
+
+  coordinatesArray[i].randomAssignment = "Duty to be extended, check back here for approximate time.";
+  
+}
+
+
+}
