9e0ed9e1cf9222f27dba0c378c50eb39d561293c
Edward J.E Koch <visceral910@gmail.com>
2016-11-11 20:58:48 -0800

    Overriding prototypal methods with valueOf and toString.

--- Functional_JS/programs_notes/overriding_prototypal_methods.js
+++ Functional_JS/programs_notes/overriding_prototypal_methods.js
@@ -1,7 +1,9 @@
 //Overriding prototypal methods.
 
-var Population (city,country){
+var Population = function (city,country){
+	//We built this city on rock and roll.
 	this.city = city;
+	//These colours don't run 
 	this.country = country;
 }
 
@@ -11,9 +13,26 @@ var city = new Population(cities,"Japan");
 
 Population.prototype.valueOf = function() {
 	var sumOfPopulations = 0;
-	for (var i = 0; i < this.city; i++){
-		sum += this.city[i][1];
+	for (var i = 0; i < this.city.length; i++){
+	
+		sumOfPopulations += this.city[i][1];
 	}
-	return sum;
+	return sumOfPopulations;
 }
 
+Population.prototype.toString = function() {
+
+	var list = "";
+
+	for (var i = 0; i < this.city.length; i ++){
+
+		if(i<this.city.length -1){
+			list = list + this.city[i][0] + ",";
+		}
+		else{
+			list = list + " and " + this.city[i][0] + " have a combined population of " + this.valueOf()}
+		}
+		return list;
+	}
+
+city.toString()
