0da5b72e1890112c1d60a834962709e8b78823d6
Edward J.E Koch <visceral910@gmail.com>
2016-11-11 20:58:48 -0800

    Same program as relaying coordinates, only with words.

--- Functional_JS/programs_notes/word_generator.js
+++ Functional_JS/programs_notes/word_generator.js
@@ -2,5 +2,15 @@ var wordScramble = {
   x: "",
   y: "",
   z: "",
+wordsConnected: undefined,
+  sendWordsTo: function(xyz) {
+    if (this.wordsConnected == undefined) {
+      var wordArray = [];
+      wordArray.push(xyz);
+      this.wordsConnected = wordArray;
+    } else {
+      this.wordsConnected.push(xyz);
+    }
+  }
+};
  
-}
