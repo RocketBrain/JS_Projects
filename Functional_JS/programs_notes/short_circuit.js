6ce613706f4c5b4b2b205ad144372a7419d2da58
Edward J.E Koch <visceral910@gmail.com>
2016-11-25 23:51:59 -0800

    Short circuiting.

---
+++ Functional_JS/programs_notes/short_circuit.js
@@ -0,0 +1,16 @@
+//Short circuit, answer becomes undefined.
+var answer = undefined && 21;
+console.log(answer);
+
+//Short circuit, answer2 becomes 0.
+var answer2 = 0 && [1,2,3];
+console.log(answer2);
+
+//Short circuit, answer3 becomes ""
+var answer3 = "" && 15
+console.log(answer3);
+
+
+
+
+
