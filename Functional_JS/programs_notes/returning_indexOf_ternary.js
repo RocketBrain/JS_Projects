6ef6b65a134c1bdc4eeaa1638759161c6a56ae21
Edward J.E Koch <visceral910@gmail.com>
2016-11-29 23:51:37 -0800

    Returning ternaries without conditionals. To be expanded at a later date.

--- Functional_JS/programs_notes/returning_indexOf_ternary.js
+++ Functional_JS/programs_notes/returning_indexOf_ternary.js
@@ -1,7 +1,14 @@
 cars = {
-	makes:["Honda","Toyota","BMW"];
+	makes:["Honda","Toyota","BMW"],
 }
 
 cars.carMake = function(make){
-	return this.makes.indexOf(make) >= 0) ?
+	return (this.makes.indexOf(make) >= 0) ? this.makes.indexOf(make) : false
 }
+	
+cars.carMake("Toyota")
+
+/*It appears that a condition must be met for the ternary to be returned,
+even though I have seen examples of a ternary being returned without any given conditions.
+The mystery deepens.
+*/
