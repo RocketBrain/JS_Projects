70a4060700459eafbca7ad9d8b8809f71d06970f
Edward J.E Koch <visceral910@gmail.com>
2016-11-30 23:50:45 -0800

    A ternary must be met with a conditional.

--- Functional_JS/programs_notes/returning_indexOf_ternary.js
+++ Functional_JS/programs_notes/returning_indexOf_ternary.js
@@ -1,14 +1,11 @@
 cars = {
-	makes:["Honda","Toyota","BMW"],
+	makes:["Honda","Toyota","BMW"]
 }
 
 cars.carMake = function(make){
-	return (this.makes.indexOf(make) >= 0) ? this.makes.indexOf(make) : false
+	return (this.makes.indexOf(make)>=0) ? //A condition must be met as the ? cannot hold a value as previously thought.
+	this.makes.splice(this.makes.indexOf(make))[0] :
+	alert("Unfortunately we do not have a " + make + " in stock.");
 }
-	
-cars.carMake("Toyota")
 
-/*It appears that a condition must be met for the ternary to be returned,
-even though I have seen examples of a ternary being returned without any given conditions.
-The mystery deepens.
-*/
+cars.carMake("Mercedes-Benz");
