6957bbd2648714df5a792393542674ed08cb249f
Edward J.E Koch <visceral910@gmail.com>
2016-12-12 21:50:35 -0800

    Switch.

---
+++ Functional_JS/programs_notes/using_switch.js
@@ -0,0 +1,21 @@
+function carMake(name) {
+
+	this.name = name;
+}
+
+switch (name) {
+
+	case 1:
+	this.car = "BMW"
+	break;
+	
+	case 2: 
+	this.car = "Toyota"
+	break;
+	  
+	case 3:
+	this.car = "Honda"
+	break;
+}
+
+
